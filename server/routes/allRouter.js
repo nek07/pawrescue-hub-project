2195087abed9ee25816930bfca6f9cf4871582dc
nek07 <abylaiooaooa@mail.ru>
2024-02-21 01:12:42 +0600

    paypal payment demo

--- server/routes/allRouter.js
+++ server/routes/allRouter.js
@@ -6,6 +6,71 @@ router.set('views', __dirname);
 const Animal = require('../models/animal');
 const middleware = require('../middleware/authMiddleware')
 const fs = require('fs')
+const paypal = require("@paypal/checkout-server-sdk")
+const Environment =
+  process.env.NODE_ENV === "production"
+    ? paypal.core.LiveEnvironment
+    : paypal.core.SandboxEnvironment
+const paypalClient = new paypal.core.PayPalHttpClient(
+  new Environment(
+    process.env.PAYPAL_CLIENT_ID,
+    process.env.PAYPAL_CLIENT_SECRET
+  )
+)
+
+const storeItems = new Map([
+  [1, { price: 100, name: "Learn React Today" }],
+  [2, { price: 200, name: "Learn CSS Today" }],
+])
+
+router.get("/", (req, res) => {
+  res.render("../public/support1", {
+    paypalClientId: process.env.PAYPAL_CLIENT_ID,
+  })
+})
+
+router.post("/support1", async (req, res) => {
+  const request = new paypal.orders.OrdersCreateRequest()
+  const total = req.body.items.reduce((sum, item) => {
+    return sum + storeItems.get(item.id).price * item.quantity
+  }, 0)
+  request.prefer("return=representation")
+  request.requestBody({
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        amount: {
+          currency_code: "USD",
+          value: total,
+          breakdown: {
+            item_total: {
+              currency_code: "USD",
+              value: total,
+            },
+          },
+        },
+        items: req.body.items.map(item => {
+          const storeItem = storeItems.get(item.id)
+          return {
+            name: storeItem.name,
+            unit_amount: {
+              currency_code: "USD",
+              value: storeItem.price,
+            },
+            quantity: item.quantity,
+          }
+        }),
+      },
+    ],
+  })
+
+  try {
+    const order = await paypalClient.execute(request);
+    res.json({ id: order.id });
+  } catch (e) {
+    console.error("Error creating PayPal order:", e);
+    res.status(500).json({ error: e.message });
+  }})
 
 
 const filePath = path.join(__dirname, '../articles1.json');
